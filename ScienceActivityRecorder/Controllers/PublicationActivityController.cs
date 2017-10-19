b4d4db9853eb9719048a84a79e13d8bb01ac7ee6
Volodymyr Iakovenko <vayak38@mail.ru>
2017-10-19 21:53:37 +0200

    Updated navigation file structure.

---
+++ ScienceActivityRecorder/Controllers/PublicationActivityController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScienceActivityRecorder.Controllers
+{
+    public class PublicationActivityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScienceActivityRecorder/Controllers/ReportsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScienceActivityRecorder.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Generate()
+        {
+            return View();
+        }
+
+        public IActionResult Visualize()
+        {
+            return View();
+        }
+    }
+}
